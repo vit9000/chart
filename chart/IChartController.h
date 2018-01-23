6ee326bdfc00522e5ff74c9df86fabb20038944d
vit9000 <vit9000@mail.ru>
2018-01-23 23:48:39 +0300

    Теперь не содержит Модель

--- chart/IChartController.h
+++ chart/IChartController.h
@@ -1,17 +1,16 @@
 #pragma once
 #include <string>
-#include "CMainModel.h"
+//#include "CMainModel.h"
 
 using namespace std;
 class IChartController
 {
 protected:
-	CMainModel *model;
+	//CMainModel *model;
 public:
-	IChartController(CMainModel* Model):model(Model){}
-	CMainModel* getModel() {return model;}
+	
 	virtual ~IChartController() {}
 	virtual void objectMouseUp(int id) = 0;
 	virtual void setPatient(size_t index)=0;
-	virtual void addDrug(const wstring& DrugName)=0;
+	virtual void addDrug()=0;
 };
