5d9c47417daf546c8c6ab4dcc00dab6c6366ba71
vit9000 <vit9000@mail.ru>
2018-01-23 18:57:31 +0300

    Интеграция Patient с Unit

--- chart/Patient.h
+++ chart/Patient.h
@@ -2,19 +2,32 @@
 #include <vector>
 #include <string>
 using namespace std;
-#include "Unit.h"
+#include "UnitContainer.h"
 
 class Patient
 {
 private:
 	wstring patient_name;
-	vector<Unit> administrations;
+	vector<UnitContainer> administrations;
 public:
-	Patient(wstring patientName)
+	Patient(const wstring& patientName)
 		: patient_name(patientName)
 	{}
 	
-	 wstring& getName() 
-	{ return patient_name;}
+	const wstring& getName() const
+	{ return patient_name; }
+
+	size_t addDrug(const wstring& DrugName)
+	{
+		administrations.push_back(UnitContainer(DrugName));
+		return administrations.size()-1;
+	}
+
+	bool addDrugDose(size_t i, const Unit& dose)
+	{
+		if(i>=administrations.size()) return false;
+		administrations[i].addUnit(dose);
+		return true;
+	}
 
 };
