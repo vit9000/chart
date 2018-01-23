08f632ddbf29fdcc45ea717cca338f049255ba76
vit9000 <vit9000@mail.ru>
2018-01-23 19:00:37 +0300

    Изменение прорисовки

--- chart/CTableLine.h
+++ chart/CTableLine.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <string>
-using std::string;
-
+using std::wstring;
 
 #include "CTableObject.h"
 #include "dpix.h"
@@ -9,21 +8,26 @@ using std::string;
 class CTableLine : public CTableObject
 {
 protected:
-	string header;
+	wstring header;
 	int border;
 
 public:
 	CTableLine(int ID, IChartController* Controller, const Rect& rectangle)
-		:CTableObject(ID, Controller, rectangle), 
-		header("String name")
+		:CTableObject(ID, Controller, rectangle)
+
 	{
+		//CMainModel* model = Controller->getModel();
+
 		border = static_cast<int>(2. * DPIX());
 	}
 
 	virtual void OnPaint(UGC& ugc)
 	{
-		ugc.SetDrawColor(0,0,0);
-		ugc.DrawRectangle(rect.x + border, rect.y + border, rect.width - border*2, rect.height - border*2,static_cast<int>(2*DPIX()));		
+		ugc.SetDrawColor(100,100,0,0);
+		ugc.FillRectangle(rect.x, rect.y, rect.reserved, rect.height);
+		ugc.SetDrawColor(100,0,100,0);
+		ugc.FillRectangle(rect.x+rect.reserved, rect.y, rect.width-rect.reserved, rect.height);
+		//ugc.DrawRectangle(rect.x + border, rect.y + border, rect.width - border*2, rect.height - border*2,static_cast<int>(2*DPIX()));		
 	}
 
 	virtual void Resize(const Rect& rectangle)
