8a6d5e4be396847b1463e9c73f2c8de7a406b209
vit9000 <vit9000@mail.ru>
2018-01-26 16:27:25 +0300

    Обработан ввод препарата

--- chart/NewLineDialog.cpp
+++ chart/NewLineDialog.cpp
@@ -28,7 +28,47 @@ void NewLineDialog::DoDataExchange(CDataExchange* pDX)
 
 
 BEGIN_MESSAGE_MAP(NewLineDialog, CDialog)
+	ON_BN_CLICKED(IDOK, &NewLineDialog::OnOKButtonClick)
 END_MESSAGE_MAP()
 
+BOOL NewLineDialog::OnInitDialog()
+{
+	CDialog::OnInitDialog();
+
+	mDrugCombo.AddString(L"NaCl 0.9%");
+	mDrugCombo.AddString(L"Glucosae 5%");
+	mDrugCombo.AddString(L"Glucosae 10%");
+	mDrugCombo.AddString(L"Венофундин");
+	mDrugCombo.AddString(L"Альбумин");
+	mDrugCombo.AddString(L"Аминоплазмаль");
+	mDrugCombo.AddString(L"KCl 4%");
+	mDrugCombo.AddString(L"MgSO4 25%");
+	mDrugCombo.AddString(L"Insulini");
+
+
+
+
+	return TRUE;  // return TRUE  unless you set the focus to a control
+}
 
 // NewLineDialog message handlers
+wstring NewLineDialog::getString()
+{
+	
+	return drugName;
+}
+void NewLineDialog::OnOKButtonClick()
+{
+	const int size = 128;
+	wchar_t buf[size];
+	mDrugCombo.GetWindowTextW(buf, size);
+	if(wcslen(buf)<=0)
+	{
+		MessageBox(L"Необходимо выбрать или ввести название препарата", L"Внимание");
+		return;
+	}
+	
+	drugName = wstring(buf);
+
+	OnOK();
+}
--- chart/NewLineDialog.h
+++ chart/NewLineDialog.h
@@ -10,22 +10,21 @@ using std::wstring;
 class NewLineDialog : public CDialog
 {
 	DECLARE_DYNAMIC(NewLineDialog)
-
+	wstring drugName;
 public:
 	NewLineDialog(CWnd* pParent = NULL);   // standard constructor
 	virtual ~NewLineDialog();
 	
-	wstring getString()
-	{
-		return L"NaCl 0.9%!";
-	}
+	wstring getString();
 // Dialog Data
 	enum { IDD = IDD_NEW_LINE_DIALOG };
 
 protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
-
+	virtual BOOL OnInitDialog();
 	DECLARE_MESSAGE_MAP()
 public:
 	CComboBox mDrugCombo;
+public:
+	afx_msg void OnOKButtonClick();
 };
