ca7604ec8df11d444cfdca932e933fa8f861643f
vit9000 <vit9000@mail.ru>
2018-02-25 23:35:27 +0300

    Исправлен баг с неправильным повторным выбором

--- chart/TableParameter.h
+++ chart/TableParameter.h
@@ -21,20 +21,18 @@ public:
 
 		ugc.SetTextSize(10);
 		double minuteW = static_cast<double>((rect.Width() - rect.reserved) / 1440.);
+		ugc.SetDrawColor(0, 0, 0);
+		ugc.SetAlign(UGC::CENTER);
 		for (const auto& unit : unitContainer->getUnits())
 		{
 			int x = rect.x + rect.reserved;
 			x += static_cast<int>(unit.getStart()*minuteW);
 			int duration = static_cast<int>(unit.getDuration()*minuteW);
-			ugc.SetDrawColor(100, 245, 245, 155);
-			ugc.FillRectangle(x,
-				rect.y,
-				duration,
-				rect.height);
-			ugc.SetDrawColor(0, 0, 0);
-			ugc.DrawNumber(unit.getValue(), x, rect.y);
+			
+			ugc.DrawNumber(unit.getValue(), x+duration/2, rect.y+rect.height/2 - ugc.GetTextHeight()/2);
 		}
 
+		ugc.SetAlign(UGC::LEFT);
 	}
 
 	bool OnLButtonUp(int x, int y) override
@@ -47,12 +45,12 @@ public:
 				{
 					x -= rect.reserved;
 					double bitW = (rect.Width() - rect.reserved) / 24.;
-					int minute = static_cast<int>(x / bitW) * 60;
+					int minute = static_cast<int>(x / bitW * 60);
 					int unitN = unitContainer->find(minute);
 					if (unitN >= 0)
 						controller->updateUnitValue(id, unitN);
 					else
-						controller->addParameterUnit(id, static_cast<int>(minute));
+						controller->addParameterUnit(id, minute/60*60);
 				}
 				else
 					controller->objectMouseUp(id);
