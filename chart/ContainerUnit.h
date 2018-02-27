f73639694121052b2bf1fbe3725611ff47c43b7e
vit9000 <vit9000@mail.ru>
2018-02-28 02:32:21 +0300

    Переделана аллокация и перемещение объектов (капельное введение и дозатором)

--- chart/ContainerUnit.h
+++ chart/ContainerUnit.h
@@ -26,7 +26,17 @@ public:
 	void rename(const wstring& NewName){ name = NewName; }
 	virtual void addUnit(const Unit& NewUnit) 
 	{ 
-		units.push_back(Unit(NewUnit)); 
+		Unit unit(NewUnit);
+
+		int start = unit.getStart();
+		int duration = unit.getDuration();
+		allocateUnit(units.size(), start, duration);
+		if (start >= 1440) return;
+
+		unit.setStart(start);
+		unit.setDuration(duration);
+
+		units.push_back(unit); 
 		sort();
 	}
 	void removeUnit(size_t) {}
@@ -45,8 +55,42 @@ public:
 	}
 	virtual void updateUnit(int index, const Unit& unit)
 	{
+		int start = unit.getStart();
+		int duration = unit.getDuration();
+		
+		allocateUnit(index, start, duration);
+		if (start >= 1440) return;
+
 		units[index] = unit;
+		units[index].setStart(start);
+		units[index].setDuration(duration);
 		sort();
+	}
+	virtual void allocateUnit(size_t index, int& start, int& duration)
+	{
+		if (duration < Unit::MIN_DURATION)
+			duration = Unit::MIN_DURATION;
+		else if (duration > Unit::MAX_DURATION)
+			duration = Unit::MAX_DURATION - 1;
+		for (size_t i = 0; i < units.size(); i++)
+		{
+			if (i == index) continue;
+			if (units[i] == start)
+				start = units[i].getStart() + units[i].getDuration() + 1;
+			else
+			{
+				int temp = duration;
+				if (start < units[i].getStart() && units[i].getStart() < start + duration)
+					temp = units[i].getStart() - start - 1;
+				if (temp < Unit::MIN_DURATION)
+				{
+					start = units[i].getStart() + units[i].getDuration() + 1;
+					i = 0;
+				}
+				else
+					duration = temp;
+			}
+		}
 	}
 
 	int find(int minute) const
