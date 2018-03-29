3aafa78a3202892b825d1b45a35ff14399eecff2
vit9000 <vit9000@mail.ru>
2018-03-29 16:23:50 +0300

    StringConverter теперь может конвертировать в обе стороны

--- chart/utils.h
+++ chart/utils.h
@@ -7,19 +7,38 @@ class StringConverter
 {
 	std::wstring local_string;
 public:
-	StringConverter(const std::string& string)
+	StringConverter(const std::string& str)
 	{
 		//CP_ACP 
-		DWORD dwNum = MultiByteToWideChar(CP_ACP, 0, string.c_str(), -1, NULL, 0);
+		/*DWORD dwNum = MultiByteToWideChar(CP_ACP, 0, string.c_str(), -1, NULL, 0);
 		wchar_t *pwText;
 		pwText = new wchar_t[dwNum];
 		MultiByteToWideChar(CP_ACP, 0, string.c_str(), -1, pwText, dwNum);
 		local_string = pwText;
-		delete[] pwText;
+		delete[] pwText;*/
+		int size_needed = MultiByteToWideChar(CP_ACP, 0, &str[0], (int)str.size(), NULL, 0);
+		std::wstring wstrTo(size_needed, 0);
+		MultiByteToWideChar(CP_ACP, 0, &str[0], (int)str.size(), &wstrTo[0], size_needed);
+		local_string = wstrTo;
 		
 	}
+
+	StringConverter(const std::wstring& str)
+	{
+		local_string = str;
+	}
+
 	operator std::wstring()
 	{
 		return local_string;
+	}
+
+	operator std::string ()
+	{
+		if (local_string.empty()) return std::string();
+		int size_needed = WideCharToMultiByte(CP_ACP, 0, &local_string[0], (int)local_string.size(), NULL, 0, NULL, NULL);
+		std::string strTo(size_needed, 0);
+		WideCharToMultiByte(CP_ACP, 0, &local_string[0], (int)local_string.size(), &strTo[0], size_needed, NULL, NULL);
+		return strTo;
 	}
 };
