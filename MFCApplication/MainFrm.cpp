6f33ffaee91825a4f12c090ce054cb451fdde62d
vit9000 <vit9000@mail.ru>
2018-05-10 19:12:32 +0300

    Подключаем библиотеку Ариадны

---
+++ MFCApplication/MainFrm.cpp
@@ -0,0 +1,37 @@
+// MainFrm.cpp : implementation of the CMainFrame class
+//
+/////////////////////////////////////////////////////////////////////////////
+#include "stdafx.h"
+//#include "tags.h"
+//#include "ArmMoving.h"
+#include "MainFrm.h"
+//#include "Global.h"
+//#include "AGrid.h"
+//#include "ADOConnection.h"
+//#include "LogonDlg.h"
+//#include "PropertiesDlg.h"
+//#include "CustomSelectDataDlg.h"
+//#include "ArmVersion.h"
+//#include "ChangePasswordDlg.h"
+#include "resource.h"
+
+/*
+
+IMPLEMENT_DYNAMIC(CMainFrame, CMainFrmBase)
+
+
+
+CMainFrame::CMainFrame() : CMainFrmBase(IDR_MAINFRAME, IDB_LOGON)
+{
+
+}
+
+CMainFrame::~CMainFrame() {}
+
+BOOL CMainFrame::_CreateUnic()
+{
+
+
+	return TRUE;
+}
+*/
