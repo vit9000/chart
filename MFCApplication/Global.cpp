647cf7340e0636ccf37befb85acad5a340c42f87
vit9000 <vit9000@mail.ru>
2018-05-14 15:17:50 +0300

    Вынесены переменные в глобальное пространство

---
+++ MFCApplication/Global.cpp
@@ -0,0 +1,13 @@
+
+#include "stdafx.h"
+#include "Global.h"
+#include "AppString.h"
+#include <atlbase.h>
+
+//////////////////////////////////////////////////////
+//  global variables
+
+CString g_def_dep_out;
+
+//CString gDayChangeTime = "";
+CADOConnection* g_lpConn = NULL;
